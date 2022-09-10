6154b2429d9cd867fff1b16a14e78898d0120447
DuudeXXI <106966677+DuudeXXI@users.noreply.github.com>
2022-09-10 21:50:00 +0300

    redoagain

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="todo-app">
+    </div>
+  );
+}
+
+export default App;
