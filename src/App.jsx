b4d5862e019c5f5411a829d51518df609ccaa071
DuudeXXI <106966677+DuudeXXI@users.noreply.github.com>
2022-09-13 14:12:39 +0300

    updt

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
-import './App.css';
+import './App.scss';
 import TodoList from './Components/TodoList';
+import Themes from './Components/Themes';
 
 function App() {
 
   return (
+    <>
     <div className="todo-app">
+      <Themes/>
       <TodoList/>
     </div>
+    </>
   );
 }
 
